f8c2698c7fd2c43be6cb16ad3f738116877c65d6
Mordecai K <mordecai.kipngetich@gmail.com>
2022-11-15 15:51:27 +0300

    update: default to yes in rpm fusion

--- quick-setup/fedora.sh
+++ quick-setup/fedora.sh
@@ -11,7 +11,7 @@ sudo dnf update -y
 
 echo "ENABLING RPM FUSION..."
 sudo dnf install https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm -y
-sudo dnf groupupdate core
+sudo dnf groupupdate core -y
 
 echo "ADDING FLATHUB REPO..."
 flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
