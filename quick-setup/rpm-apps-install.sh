b593644c17cf2c128d2c09614ba1be2521a1e508
Mordecai <mordecai.kipngetich@gmail.com>
2022-11-15 19:15:31 +0300

    update: capitalise title and space out

--- quick-setup/rpm-apps-install.sh
+++ quick-setup/rpm-apps-install.sh
@@ -1,15 +1,17 @@
 #!/bin/bash
 
-echo "Installing Tweaks, Alacritty, Steam, htop, gThumb, Solaar"
+echo "INSTALLING GNOME TWEAKS, ALACRITTY, STEAM, HTOP, GTHUMB AND SOLAAR"
 sudo dnf install -y gnome-tweaks alacritty steam htop gthumb solaar
+echo ""
 
-echo "Installing VS Code"
+echo "INSTALLING VS CODE"
 sudo rpm --import https://packages.microsoft.com/keys/microsoft.asc
 sudo sh -c 'echo -e "[code]\nname=Visual Studio Code\nbaseurl=https://packages.microsoft.com/yumrepos/vscode\nenabled=1\ngpgcheck=1\ngpgkey=https://packages.microsoft.com/keys/microsoft.asc" > /etc/yum.repos.d/vscode.repo'
 sudo dnf update -y
 sudo dnf install code -y
+echo ""
 
-echo "Installing nautilus-open-any-terminal and starship - to work with Alacritty"
+echo "INSTALLING NAUTILUS-OPEN-ANY-TERMINAL AND STARSHIP - TO WORK WITH ALACRITTY"
 sudo dnf install nautilus-python -y
 sudo dnf install pip -y
 pip install nautilus-open-any-terminal
@@ -24,21 +26,24 @@ gsettings set com.github.stunkymonkey.nautilus-open-any-terminal flatpak system
 cd ..
 rm nautilus-open-any-terminal -rf
 sudo dnf install starship
+echo ""
 
-echo "Installing Brave Browser"
+echo "INSTALLING BRAVE BROWSER"
 sudo dnf install dnf-plugins-core -y
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
 sudo rpm --import https://brave-browser-rpm-release.s3.brave.com/brave-core.asc
 sudo dnf install brave-browser -y
+echo ""
 
-echo "Installing XDM"
+echo "INSTALLING XDM"
 curl -LO https://github.com/subhra74/xdm/releases/download/7.2.11/xdm-setup-7.2.11.tar.xz
 mkdir xdm-setup-dir
 tar -xf xdm-setup*tar.xz -C xdm-setup-dir/
 sudo ./xdm-setup-dir/install.sh
 rm xdm-setup* -rf
+echo ""
 
-echo "Installing ProtonVPN"
+echo "INSTALLING PROTONVPN"
 sudo rpm -i https://repo.protonvpn.com/fedora-36-stable/release-packages/protonvpn-stable-release-1.0.1-1.noarch.rpm
 sudo dnf update -y
 sudo dnf install protonvpn -y
