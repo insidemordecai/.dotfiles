24581b233aa63dc47bf19cc0349c9b8306d2b2ab
Mordecai K <mordecai.kipngetich@gmail.com>
2022-11-15 15:17:10 +0300

    update: automate extra installations
    nautilus-open-any-terminal and starship

--- quick-setup/rpm-apps-install.sh
+++ quick-setup/rpm-apps-install.sh
@@ -3,6 +3,16 @@
 echo "Installing Tweaks, Alacritty, VS Code, Steam, htop, gThumb, Solaar"
 sudo dnf install -y gnome-tweaks alacritty code steam htop gthumb solaar
 
+echo "Installing nautilus-open-any-terminal and starship - to work with Alacritty"
+sudo dnf install nautilus-python -y
+pip install nautilus-open-any-terminal
+nautilus -q	# kill nautilus to load new extension
+gsettings set com.github.stunkymonkey.nautilus-open-any-terminal terminal alacritty
+gsettings set com.github.stunkymonkey.nautilus-open-any-terminal keybindings '<Ctrl><Alt>t'
+gsettings set com.github.stunkymonkey.nautilus-open-any-terminal new-tab true
+gsettings set com.github.stunkymonkey.nautilus-open-any-terminal flatpak system
+sudo dnf install starship
+
 echo "Installing Brave Browser"
 sudo dnf install dnf-plugins-core -y
 sudo dnf config-manager --add-repo https://brave-browser-rpm-release.s3.brave.com/x86_64/
