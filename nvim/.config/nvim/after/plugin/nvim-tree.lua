96d7e7f67de52ec22f78f16c46fd00b0e4180b88
Mordecai K <39312305+insidemordecai@users.noreply.github.com>
2023-01-17 12:11:48 +0300

    update: slight tweak to nvim-tree config

--- nvim/.config/nvim/after/plugin/nvim-tree.lua
+++ nvim/.config/nvim/after/plugin/nvim-tree.lua
@@ -1,9 +1,15 @@
--- must be set before calling require
+local setup, nvimtree = pcall(require, "nvim-tree")
+if not setup then
+  return
+end
+
 vim.g.nvim_tree_indent_markers = 1 -- this option shows indent markers when folders are open
+
+-- recommended according to nvim-tree docs
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
-require('nvim-tree').setup {
+nvimtree.setup {
     view = {
         adaptive_size = true,
     }
