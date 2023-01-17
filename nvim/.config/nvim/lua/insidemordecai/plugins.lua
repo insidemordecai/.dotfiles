12aa53669ab8a847f45f87c177f7e25b0b35f2ec
Mordecai K <39312305+insidemordecai@users.noreply.github.com>
2023-01-17 15:23:36 +0300

    update: packer autoreload autocmd

--- nvim/.config/nvim/lua/insidemordecai/plugins.lua
+++ nvim/.config/nvim/lua/insidemordecai/plugins.lua
@@ -16,7 +16,7 @@ local packer_bootstrap = ensure_packer() -- true if packer was just installed
 vim.cmd([[ 
   augroup packer_user_config
     autocmd!
-    autocmd BufWritePost plugins-setup.lua source <afile> | PackerSync
+    autocmd BufWritePost plugins.lua source <afile> | PackerSync
   augroup end
 ]])
 
