a7c97c201c0086533dda2aa0cd25ebad5095d279
Mordecai K <39312305+insidemordecai@users.noreply.github.com>
2023-07-10 11:15:39 +0300

    update steam install: rpm -> flatpak

--- quick-setup/rpm-apps-install.sh
+++ quick-setup/rpm-apps-install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-echo "INSTALLING GNOME TWEAKS, ALACRITTY, STEAM, HTOP, GTHUMB, SOLAAR, NVIM, AUTOJUMP HUGO TRASH"
-sudo dnf install -y gnome-tweaks alacritty steam htop gthumb solaar nvim autojump hugo trash-cli
+echo "INSTALLING GNOME TWEAKS, ALACRITTY, HTOP, GTHUMB, SOLAAR, NVIM, AUTOJUMP HUGO TRASH"
+sudo dnf install -y gnome-tweaks alacritty htop gthumb solaar nvim autojump hugo trash-cli
 echo ""
 
 echo "INSTALLING VS CODE"
