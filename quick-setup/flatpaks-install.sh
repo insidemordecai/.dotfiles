2fdad3a4986545138d87b75f1523a242db5ae67f
Mordecai K <39312305+insidemordecai@users.noreply.github.com>
2023-07-10 11:26:31 +0300

    update gthumb installation to flatpak

--- quick-setup/flatpaks-install.sh
+++ quick-setup/flatpaks-install.sh
@@ -2,7 +2,7 @@
 
 echo "Installing Flatpak Apps"
 
-flatpak install flathub com.mattjakeman.ExtensionManager org.mozilla.firefox com.spotify.Client org.videolan.VLC org.qbittorrent.qBittorrent com.github.iwalton3.jellyfin-media-player com.valvesoftware.Steam -y
+flatpak install flathub com.mattjakeman.ExtensionManager org.mozilla.firefox com.spotify.Client org.videolan.VLC org.qbittorrent.qBittorrent com.github.iwalton3.jellyfin-media-player com.valvesoftware.Steam org.gnome.gThumb -y
 
 echo "Setting Wayland as display server for Firefox"
 sudo flatpak override --env=MOZ_ENABLE_WAYLAND=1
