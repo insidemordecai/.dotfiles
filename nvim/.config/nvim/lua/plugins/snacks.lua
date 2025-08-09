30cbb1a2a928d37144c0c202bb9835c3e8d7e559
Mordecai K <39312305+insidemordecai@users.noreply.github.com>
2025-08-10 00:47:56 +0300

    update(lazyvim): show hidden files

---
+++ nvim/.config/nvim/lua/plugins/snacks.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "folke/snacks.nvim",
+    opts = {
+      picker = {
+        hidden = true, -- show hidden files in explorer
+        sources = {
+          files = {
+            hidden = true, -- show hidden files in find
+          },
+        },
+      },
+    },
+  },
+}
